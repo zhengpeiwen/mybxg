9e14cc74435338f579c2f9b34e108efd86923ec5
zhengpeiwen <821894829@qq.com>
2017-09-22 20:00:14 +0800

    添加表单验证和表单提交插件

--- public/js/config.js
+++ public/js/config.js
@@ -5,6 +5,10 @@ require.config({
 		cookie : 'jquery-cookie/jquery.cookie',
 		template : 'artTemplate/template-web',
 		bootstrap:'bootstrap/js/bootstrap',
+		datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
+		language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+		validate:'validate/jquery-validate.min',
+		form:'jquery-form/jquery.form',
 		util:'../js/util',
 		common : '../js/common',
 		login : '../js/login',
@@ -13,6 +17,12 @@ require.config({
 	},
 	shim:{
 		bootstrap:{
+			deps:['jquery']
+		},
+		language:{
+			deps:['jquery','datepicker']
+		},
+		validate:{
 			deps:['jquery']
 		}
 	}
