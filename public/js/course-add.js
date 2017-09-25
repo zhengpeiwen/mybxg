7c7af9d3ef9a116736c59c92a6cf8d3481aa7f3f
zhengpeiwen <821894829@qq.com>
2017-09-25 17:21:45 +0800

    课程添加和编辑共享页面

---
+++ public/js/course-add.js
@@ -0,0 +1,17 @@
+define(['jquery','template','util','form'], function ($,template,util) {
+    //设置导航菜单选中
+    util.setMenu(location.pathname);
+    //绑定事件
+    $('#courseBtn').click(function () {
+       $('#courseForm').ajaxSubmit({
+           type:'post',
+           url:'/api/course/create',
+           dataType:'json',
+           success: function (data) {
+               if(data.code==200){
+                   location.href='/course/basic?cs_id='+data.result.cs_id;
+               }
+           }
+       });
+    });
+});
